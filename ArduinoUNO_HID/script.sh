0d6efb4e14a31f12073a671c8b68acf0b7ef6d84
PepeBigotes <107816872+PepeBigotes@users.noreply.github.com>
2022-10-30 00:14:38 +0200

    Update script.sh

--- ArduinoUNO_HID/script.sh
+++ ArduinoUNO_HID/script.sh
@@ -21,7 +21,7 @@ echo "Resetting ARDUINO to default firmware..."
 dfu-programmer atmega16u2 erase
 dfu-programmer atmega16u2 flash --suppress-bootloader-mem Arduino-COMBINED-dfu-usbserial-atmega16u2-Uno-Rev3.hex
 dfu-programmer atmega16u2 reset
-echo "1 - Unplug the ARDUINO from the PC and plug it in again."
+echo "1 - Unplug the ARDUINO from the PC and plug it again."
 echo "2 - Upload the code into ARDUINO using Arduino IDE"
 echo "3 - Connect two pins to reset ARDUINO"
 echo "4 - Remove the jumper that connect two pins"
