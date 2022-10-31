9f84325e3ea5741613f2fd3cad00187fd06a134f
Pepe <yarozbond@gmail.com>
2022-10-31 16:36:51 +0100

    Small change in script refering the new 'the2pins.png'

--- ArduinoUNO_HID/script.sh
+++ ArduinoUNO_HID/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cat ../logo.txt
 echo "1 - Plug ARDUINO UNO into your PC with the two pins connected."
-echo "    (Check the 'the2pins.png' if you have no idea what these pins are)"
+echo "    (Check 'the2pins.png' if you have no idea what these pins are)"
 echo "2 - Remove the jumper that connect pins."
 echo "3 - Press ENTER"
 read
